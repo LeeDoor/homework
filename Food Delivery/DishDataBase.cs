848cb970881eb52bd21de6f423b33857c661ddf8
Leonid <samoshenkoleonid@yandex.ru>
2022-06-11 13:44:23 +0300

    databaseFill

--- Food Delivery/DishDataBase.cs
+++ Food Delivery/DishDataBase.cs
@@ -18,6 +18,14 @@ namespace Delivery
         {
             new Dish(0, "Банан", 15, 25, false),
             new Dish(1, "Огурец", 12, 31,  false),
+            new Dish(1, "брынза", 266, 262,  false),
+            new Dish(1, "йогурт", 60, 57,  false),
+            new Dish(1, "кефир", 70, 40,  true),
+            new Dish(1, "молоко", 100, 45,  true),
+            new Dish(1, "сметана", 64, 119,  false),
+            new Dish(1, "каша овсянная", 30, 333,  false),
+            new Dish(1, "каша рисовая", 32, 333,  false),
+            new Dish(1, "фасолевый суп", 200, 54,  false),
             new Dish(2, "Апельсиновый сок 0,5л", 70, 200, true)
         };
 
